94afdd4aef93d7d5d87c0b5eeb83ae26368f1675
bepposax <bepposax@hotmail.it>
2023-06-09 12:29:52 +0200

    ;

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ls ~/scripts 2>/dev/null || mkdir ~/scripts
-ls ~/scripts | grep update.sh || mv update.sh ~/scripts
+ls ~/scripts 2>/dev/null || mkdir ~/scripts;
+ls ~/scripts | grep update.sh || mv update.sh ~/scripts;
 
-ls ~ | grep ~/.bash_aliases 1>/dev/null || touch ~/.bash_aliases
+ls ~ | grep ~/.bash_aliases 1>/dev/null || touch ~/.bash_aliases;
 if ! grep "alias update" ~/.bash_aliases; then
-  echo "alias update='bash ~/scripts/update.sh'" >> ~/.bash_aliases
-fi
+  echo "alias update='bash ~/scripts/update.sh'" 1>/dev/null >> ~/.bash_aliases
+fi;
 
 cd .. ; rm -rf ./my_linux_scripts
