4423649e30dd00da929101be0cec7c331815754c
bepposax <bepposax@hotmail.it>
2023-06-24 18:22:09 +0200

    `test` over if statement

--- update.sh
+++ update.sh
@@ -4,12 +4,10 @@ sudo apt-get autoclean
 sudo apt update
 
 # Installa gli aggiornamenti disponibili solo se ci sono nuovi aggiornamenti
-if [ "$(apt list --upgradable 2>/dev/null | wc -l)" -gt "1" ]; then
+[[$(apt list --upgradable 2>/dev/null | wc -l) >1]] && {
   apt list --upgradable
   sudo apt upgrade -y
-fi
+}
 
 # Rimuove i pacchetti obsoleti solo se ci sono pacchetti obsoleti
-if [ "$(apt list ?obsolete 2>/dev/null | wc -l)" -gt "1" ]; then
-  sudo apt autoremove -y
-fi
+[[$(apt list ?obsolete 2>/dev/null | wc -l) >1]] && sudo apt autoremove -y
