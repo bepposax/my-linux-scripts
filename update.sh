0a71bb01619cf0c7071a9d1b0d54284d87fef25c
bepposax <bepposax@hotmail.it>
2023-06-24 18:31:46 +0200

    `test` syntax fix

--- update.sh
+++ update.sh
@@ -4,10 +4,13 @@ sudo apt-get autoclean
 sudo apt update
 
 # Installa gli aggiornamenti disponibili solo se ci sono nuovi aggiornamenti
-[[$(apt list --upgradable 2>/dev/null | wc -l) >1]] && {
+[[ $(apt list --upgradable 2>/dev/null | wc -l) > 1 ]] && {
   apt list --upgradable
   sudo apt upgrade -y
 }
 
 # Rimuove i pacchetti obsoleti solo se ci sono pacchetti obsoleti
-[[$(apt list ?obsolete 2>/dev/null | wc -l) >1]] && sudo apt autoremove -y
+[[ $(apt list ?obsolete 2>/dev/null | wc -l) > 1 ]] && {
+  apt list ?obsolete
+  sudo apt autoremove -y
+}
