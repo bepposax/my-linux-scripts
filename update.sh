723fbafce4b33ed118c2f1d0af5cbda4eca5babe
bepposax <bepposax@hotmail.it>
2023-06-24 18:05:22 +0200

    add autoclean

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+sudo apt-get autoclean
 sudo apt update
 
 # Installa gli aggiornamenti disponibili solo se ci sono nuovi aggiornamenti
-if [ "$(sudo apt list --upgradable 2>/dev/null | wc -l)" -gt "1" ]; then
-  sudo apt list --upgradable
+if [ "$(apt list --upgradable 2>/dev/null | wc -l)" -gt "1" ]; then
+  apt list --upgradable
   sudo apt upgrade -y
 fi
 
 # Rimuove i pacchetti obsoleti solo se ci sono pacchetti obsoleti
-if [ "$(sudo apt list ?obsolete 2>/dev/null | wc -l)" -gt "1" ]; then
+if [ "$(apt list ?obsolete 2>/dev/null | wc -l)" -gt "1" ]; then
   sudo apt autoremove -y
 fi
