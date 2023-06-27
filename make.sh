35401540cb46ef7bf6175c6e6c97fd69b236ac81
bepposax <bepposax@hotmail.it>
2023-06-28 00:15:07 +0200

    fix potential dangerous code

--- make.sh
+++ make.sh
@@ -1,44 +1,57 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
+DIR=$(dirname "$0")
 
 # disabling MOTD...
 HUSH=~/.hushlogin
 
-[ -f $HUSH ] && echo "'$HUSH' exists. Skipping" || {
+if [ -f $HUSH ]; then
+  echo "'$HUSH' exists. Skipping"
+else {
   echo -n "Creating '$HUSH'..."
   touch $HUSH && echo " Done"
-}
+}; fi
 
 # adding the update script...
 DEST=~/scripts
 SCRIPT=update.sh
 
 # creates DEST if it doesn't exist
-[ -d $DEST ] && echo "'$DEST' exists. Skipping" || {
+if [ -d $DEST ]; then
+  echo "'$DEST' exists. Skipping"
+else {
   echo -n "Creating '$DEST'..."
   mkdir $DEST && echo " Done"
-}
+}; fi
+
 # copies SCRIPT file in DEST if it doesn't exist
-[ -f $DEST/$SCRIPT ] && echo "'$DEST/$SCRIPT' exists. Skipping" || {
+if [ -f $DEST/$SCRIPT ]; then
+  echo "'$DEST/$SCRIPT' exists. Skipping"
+else {
   echo -n "Copying '$DIR/$SCRIPT' in '$DEST'..."
-  cp $DIR/$SCRIPT $DEST && echo " Done"
-}
+  cp "$DIR"/$SCRIPT $DEST && echo " Done"
+}; fi
 
 # setting alias to run the update script...
 ALIASFILE=~/.bash_aliases
-ALIAS="alias update='bash $DEST/$SCRIPT'"
 
 # creates ALIASFILE if it doesnìt exist
-[ -f $ALIASFILE ] && echo "'$ALIASFILE' exists. Skipping" || {
+if [ -f $ALIASFILE ]; then
+  echo "'$ALIASFILE' exists. Skipping"
+else {
   echo -n "Creating '$ALIASFILE'..."
   touch $ALIASFILE && echo " Done"
-}
+}; fi
+
 # adds the ALIAS line inside ALIASFILE if it doesn't exist
-grep "alias update" $ALIASFILE 1>/dev/null && echo "alias already set. Skipping" || {
-  echo -n "Adding \"$ALIAS\" to '$ALIASFILE'..."
-  echo $ALIAS >>$ALIASFILE && echo " Done"
-}
+ALIAS=("alias update='bash $DEST/$SCRIPT'")
+
+if grep "alias update" $ALIASFILE 1>/dev/null; then
+  echo "\"${ALIAS[*]}\" already set. Skipping"
+else {
+  echo -n "Adding \"${ALIAS[*]}\" to '$ALIASFILE'..."
+  echo "${ALIAS[@]}" >>$ALIASFILE && echo " Done"
+}; fi
 
 # adding command to ~/.bashrc...
 BASHRC=~/.bashrc
@@ -54,10 +67,12 @@ BASHRC=~/.bashrc
   CMD="PROMPT_DIRTRIM=2"
 
   # adds CMD to ~/.bashrc if it isn't set
-  grep $CMD $BASHRC 1>/dev/null && echo "$CMD already set. Skipping" || {
+  if grep $CMD $BASHRC 1>/dev/null; then
+    echo "$CMD already set. Skipping"
+  else {
     echo -n "Adding '$CMD' to '$BASHRC'..."
     echo -e "\n$CMDDOC\n$CMD" >>$BASHRC && echo " Done"
-  }
+  }; fi
 }
 
 # changing mirrors in /etc/apt/sources.list to ".it" mirrors...
@@ -65,22 +80,22 @@ SRCLST="/etc/apt/sources.list"
 NEW_MIRR="deb http://it.archive"
 
 # sets ".it" mirrors only if they're not ".it" mirrors
-grep "$NEW_MIRR" $SRCLST 1>/dev/null &&
-  echo "Mirrors in '$SRCLST' already set. Skipping" ||
-  {
-    # assigns to MIRR whichever mirror "head" is currently saved in SRCLST
-    MIRR=$(grep -Eom 1 "^deb http://([a-z]{2}.)?archive" $SRCLST)
-    echo -n "Changing mirrors in '$SRCLST'... "
-    sudo sed -i "s+$MIRR+$NEW_MIRR+g" $SRCLST && echo "Done"
-  }
+if grep "$NEW_MIRR" $SRCLST 1>/dev/null; then
+  echo "Mirrors in '$SRCLST' already set. Skipping"
+else {
+  # assigns to MIRR whichever mirror "head" is currently saved in SRCLST
+  MIRR=$(grep -Eom 1 "^deb http://([a-z]{2}.)?archive" $SRCLST)
+  echo -n "Changing mirrors in '$SRCLST'... "
+  sudo sed -i "s+$MIRR+$NEW_MIRR+g" $SRCLST && echo "Done"
+}; fi
 
 # removing the folder containing this file...
-[[ $DIR == . ]] && {
+if [[ $DIR == . ]]; then {
   echo -n "Removing '$PWD'..."
-  rm -rf $PWD && echo " Done"
-} || {
+  rm -rf "$PWD" && echo " Done"
+}; else {
   echo -n "Removing '$DIR'..."
-  rm -rf $DIR && echo " Done"
-}
+  rm -rf "$DIR" && echo " Done"
+}; fi
 
 echo "Restart the terminal for changes to take effect." && exit 0
--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ sudo apt-get autoclean
 sudo apt update
 
 # Installa gli aggiornamenti disponibili solo se ci sono nuovi aggiornamenti
-[[ $(apt list --upgradable 2>/dev/null | wc -l) > 1 ]] && {
+(($(apt list --upgradable 2>/dev/null | wc -l) > 1)) && {
   apt list --upgradable
   sudo apt upgrade -y
 }
 
 # Rimuove i pacchetti obsoleti solo se ci sono pacchetti obsoleti
-[[ $(apt list ?obsolete 2>/dev/null | wc -l) > 1 ]] && {
-  apt list ?obsolete
+(($(apt list -- ?obsolete 2>/dev/null | wc -l) > 1)) && {
+  apt list -- ?obsolete
   sudo apt autoremove -y
 }
 exit 0
