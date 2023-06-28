5a5d56980dbbd10ca5b8d6f41a4c9f0f4b25b7c9
Ivano <ivan@cube-i9.homenet.telecomitalia.it>
2023-06-28 04:21:29 +0200

    dashes over newline

--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-echo -e "\nCleaning package cache..."
+echo "Cleaning package cache..."
 sudo apt-get autoclean
-echo -e "\nChecking updates..."
+echo "---"
+echo "Checking updates..."
 sudo apt update
-
+echo "---"
 # Installa gli aggiornamenti disponibili solo se ci sono nuovi aggiornamenti
 (($(apt list --upgradeable 2>/dev/null | wc -l) > 1)) && {
-  echo -e "\nUpgrading packages..."
+  echo "Upgrading packages..."
   apt list --upgradeable
   sudo apt upgrade -y
 }
-
+echo "---"
 # Rimuove i pacchetti obsoleti solo se ci sono pacchetti obsoleti
 if (($(apt list -- ?obsolete 2>/dev/null | wc -l) > 1)); then {
-  echo -e "\nRemoving obsolete packages..."
+  echo "Removing obsolete packages..."
   apt list -- ?obsolete
   sudo apt autoremove -y
-}; else echo -e "\nNo obsolete packages found."; fi
+}; else echo "No obsolete packages found."; fi
 exit 0
