14c6fd80a0501ab72da49bb48fc0e02dd6831f99
BeppoSAX <bepposax10@gmail.com>
2023-06-29 23:22:29 +0200

    desktop icons position + some refactor

--- make.sh
+++ make.sh
@@ -92,9 +92,9 @@ grep "$NEW_MIRR" $SRCLST 1>/dev/null || {
   lsb_release -d | grep Ubuntu 1>/dev/null &&
   {
     # installing gnome-shell-extensions if not installed...
-    (($(apt list --installed gnome-shell-extensions 2>/dev/null | wc -l) <= 1)) && {
+    (($(apt list --installed gnome-shell-extensions 2>/dev/null | wc -l) > 1)) || {
       echo "Installing 'gnome-shell-extensions'..."
-      sudo apt-get -qq install gnome-shell-extensions
+      sudo apt-get -qq install gnome-shell-extensions && echo "Done"
       RELOG=true
     }
     # installing gnome-shell-extension-installer if not installed...
@@ -107,19 +107,13 @@ grep "$NEW_MIRR" $SRCLST 1>/dev/null || {
     }
     EXTPATH="$HOME/.local/share/gnome-shell/extensions"
     # installing 'dash-to-dock' gnome extension if not installed...
-    [ ! -d "$EXTPATH/dash-to-dock@micxgx.gmail.com" ] && {
+    [ -d "$EXTPATH/dash-to-dock@micxgx.gmail.com" ] || {
       echo -n "Installing 'Dash To Dock' gnome extension..."
       gnome-shell-extension-installer 307 --yes &>/dev/null && echo " Done"
       RELOG=true
     }
-    # installing 'hidetopbar' gnome extension if not installed...
-    [ ! -d "$EXTPATH/hidetopbar@mathieu.bidon.ca" ] && {
-      echo -n "Installing 'Hide Top Bar' gnome extension..."
-      gnome-shell-extension-installer 545 --yes &>/dev/null && echo " Done"
-      RELOG=true
-    }
     # turning on dock 'autohide' if not on...
-    ! eval "$(gsettings get org.gnome.shell.extensions.dash-to-dock autohide)" && {
+    eval "$(gsettings get org.gnome.shell.extensions.dash-to-dock autohide)" || {
       echo -n "Turning on dock 'autohide'..."
       gsettings set org.gnome.shell.extensions.dash-to-dock autohide true && echo " Done"
       CHANGES=true
@@ -137,22 +131,45 @@ grep "$NEW_MIRR" $SRCLST 1>/dev/null || {
       CHANGES=true
     }
     # shrinking the dock if not shrinked...
-    ! eval "$(gsettings get org.gnome.shell.extensions.dash-to-dock custom-theme-shrink)" && {
+    eval "$(gsettings get org.gnome.shell.extensions.dash-to-dock custom-theme-shrink)" || {
       echo -n "Shrinking the dock..."
       gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true && echo " Done"
       CHANGES=true
     }
     # moving dock to bottom if not there already...
-    ! gsettings get org.gnome.shell.extensions.dash-to-dock dock-position | grep 'BOTTOM' 1>/dev/null && {
+    gsettings get org.gnome.shell.extensions.dash-to-dock dock-position | grep 'BOTTOM' 1>/dev/null || {
       echo -n "Moving dock to bottom..."
       gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM' && echo " Done"
       CHANGES=true
     }
+    # setting dock transparency mode to dynamic if not different...
+    gsettings get org.gnome.shell.extensions.dash-to-dock transparency-mode | grep 'DYNAMIC' 1>/dev/null || {
+      echo -n "Changing dock transparency mode to 'DYNAMIC'..."
+      gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'DYNAMIC' && echo " Done"
+      CHANGES=true
+    }
     # changing dock transparency to 80% if different...
-    ! gsettings get org.gnome.shell.extensions.dash-to-dock background-opacity | grep 0.2 1>/dev/null && {
+    gsettings get org.gnome.shell.extensions.dash-to-dock background-opacity | grep 0.2 1>/dev/null || {
       echo -n "Changing dock transparency to 80%..."
-      gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'DYNAMIC'
       gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.2 && echo " Done"
+      CHANGES=true
+    }
+    # installing 'hidetopbar' gnome extension if not installed...
+    [ -d "$EXTPATH/hidetopbar@mathieu.bidon.ca" ] || {
+      echo -n "Installing 'Hide Top Bar' gnome extension..."
+      gnome-shell-extension-installer 545 --yes &>/dev/null && echo " Done"
+      RELOG=true
+    }
+    # installing desktop-icons-ng if not installed...
+    (($(apt list --installed gnome-shell-extension-desktop-icons-ng 2>/dev/null | wc -l) > 1)) || {
+      echo "Installing 'Desktop Icons NG (DING)'..."
+      sudo apt-get -qq install gnome-shell-extension-desktop-icons-ng && echo "Done"
+      RELOG=true
+    }
+    # setting desktop icons default position to bottom left if not already set
+    gsettings get org.gnome.shell.extensions.ding start-corner | grep 'bottom-left' 1>/dev/null || {
+      echo -n "Changing desktop icons default position to 'bottom-left'..."
+      gsettings set org.gnome.shell.extensions.ding start-corner 'bottom-left' && echo " Done"
       CHANGES=true
     }
   }
